ee4cc371f2df7f84fe6adf4945c6afec97c6f3fd
emmanuelwanaah <emmanuelwanaah@gmail.com>
2026-04-24 13:42:45 -1200

    fix: correct port binding for railway

--- server.js
+++ server.js
@@ -99,15 +99,10 @@ const db = require("./config/db");
 })();
 
 
-// Start server
-const PORT = process.env.PORT;
+
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-
-// const PORT = process.env.PORT || 8080;
-
-// app.listen(PORT, "0.0.0.0", () => {
-//   console.log(`🚀 Server running on port ${PORT}`);
-// });
